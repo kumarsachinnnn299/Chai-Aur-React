47f7fc123acad1d2d716d9bd0e4244c84e9e1f00
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-01-13 02:21:23 +0530

    Creating react project with npx and vite

---
+++ 01_npxreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React app project created with NPX || Sachin Kumar Singh</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <h1>React with Vite || Sachin Kumar Singh</h1>
+  )
+}
+
+export default App
