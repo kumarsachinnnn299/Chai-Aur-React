d4e6224d068b61536a2d67eccc5c5b450e6ec899
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-01-22 03:22:11 +0530

    Currency Converter Project: Contri 01

---
+++ 07currencyConverter/src/components/index.js
@@ -0,0 +1,8 @@
+/*Ye file bas isliye bnayi h kyuki isme hum sab components ko import krr lenge and use krne k liye bas 
+isi file ko import krr lenge . isse bas bahut saari files hme import nhi krni pdengi
+
+Kyuki components bahut saare ho skte h kisi page m to sabko individually unki files se import krne se better
+h ki is file m sabko import krke is file se sabko import krr le*/
+
+import InputBox from "./InputBox";
+export default {InputBox}
--- 07currencyConverter/src/hooks/useCurrencyInfo.js
+++ 07currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,17 @@
 import { useEffect,useState } from "react";
 
 //hooks bnana koi bdi baat nhi h. These are like some fns. They can use other react hooks also
-
+// hooks ko mainly .js rkho
 function useCurrencyInfo(currency){
-    const[data,setdata]=useState({})
+    const[data,setData]=useState({})
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res)=>res.json())
-        .then((res)=>setdata(res[currency]))
+        .then((res)=>setData(res[currency]))
 
     },[currency])
     console.log(data);
     return data
     
 }
-export default useCurrencyInfo
+export default useCurrencyInfo;
