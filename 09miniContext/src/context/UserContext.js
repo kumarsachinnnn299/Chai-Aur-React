6e0bde265fad5cff3e63cd11cccdcb43a14cf479
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-02-09 01:22:45 +0530

    Context API Discussion

---
+++ 09miniContext/src/context/UserContext.js
@@ -0,0 +1,11 @@
+import React from "react";
+const userContext=React.createContext()
+export default userContext;
+
+//Context hme variable ka access provide krega
+//Context global variable ki tarah maano
+
+/*
+Context bnane k baad uska provide bhi bnana pdega. Ye provider ek wrapper ki tarah kaam krta h
+
+*/
