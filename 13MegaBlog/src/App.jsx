79482fec7833c4d5bd13f538361ca59f7254d9fa
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-02-14 23:59:04 +0530

    Some progress: Created store

--- 13MegaBlog/src/App.jsx
+++ 13MegaBlog/src/App.jsx
@@ -1,5 +1,12 @@
 import './App.css'
 import conf from './conf/conf';
+import { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import { Header,Footer } from './components';
+import { Outlet } from 'react-router-dom';
+
 function App() {
  //To read env file
 
@@ -9,12 +16,36 @@ function App() {
 //  console.log(conf.appwriteProjectId);
 //  console.log(conf.appwriteUrl);
 
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-        
-    </>
-  )
+  const [loading,setLoading]=useState(true);
+  const dispatch=useDispatch();
+  useEffect(() => {
+      authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=>{
+        setLoading(false);
+      })
+  }, [])
+  
+
+  return loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full-block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
+  
 }
 
 export default App
